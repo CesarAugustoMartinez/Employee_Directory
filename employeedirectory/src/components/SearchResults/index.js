ebf6df61d0bc9895b78fa8c910e9222d04601e98
Cesar Martinez <cesaguma@hotmail.com>
2021-01-14 21:58:58 -0500

    Creating a tbody element into SearchResults component to show the data from the results parameter

--- employeedirectory/src/components/SearchResults/index.js
+++ employeedirectory/src/components/SearchResults/index.js
@@ -2,15 +2,51 @@ import React from "react";
 import "./style.css";
 
 function SearchResults(props) {
+
+  function formatDate(date) {
+    const dateArray = date.split("-");
+    const year = dateArray[0];
+    const month = dateArray[1];
+    const dayArray = dateArray[2].split("T");
+    const day = dayArray[0];
+    const formattedDate = [month, day, year].join("-");
+    return formattedDate;
+  }
+
   return (
-    <ul className="list-group search-results">
-      {props.results.map(result => (
-        <li key={result.email} className="list-group-item">
-          <p>{result.name.title}</p>
-          <img alt="Employee" src={result} className="img-fluid" />
-        </li>
-      ))}
-    </ul>
+    <tbody>
+      {props.results[0] !== undefined && props.results[0].name !== undefined ? (
+        props.results.map(result => {
+          return (
+            <tr key={result.email}>
+              <td data-th="Image" className="align-middle">
+                <img
+                  src={result.picture.medium}
+                  alt={"profile image for " + result.name.first + " " + result.name.last}
+                  className="img-responsive"
+                />
+              </td>
+              <td data-th="Name" className="name-cell align-middle">
+                {result.name.first} {result.name.last}
+              </td>
+              <td data-th="Phone" className="align-middle">
+                {result.phone}
+              </td>
+              <td data-th="Email" className="align-middle">
+                <a href={"mailto:" + result.email} target="__blank">
+                  {result.email}
+                </a>
+              </td>
+              <td data-th="DOB" className="align-middle">
+                {formatDate(result.dob.date)}
+              </td>
+            </tr>
+          );        
+        })
+      ) : (
+        <></>
+      )}
+    </tbody>
   );
 }
 
