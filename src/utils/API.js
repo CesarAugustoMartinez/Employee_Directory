f779d6ce44ed74bd5bb3df1538e35c7892e31433
Cesar Martinez <cesaguma@hotmail.com>
2021-01-16 00:34:47 -0500

    Updating folder

--- src/utils/API.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default {
-  getRandomEmployee: function() {
-    return axios.get("https://randomuser.me/api/?inc=id,picture,name,phone,email,dob&results=50");
-  },
-};
