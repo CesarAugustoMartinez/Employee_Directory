977a73800724e2e670baa57a58834d5fb67f546a
Cesar Martinez <cesaguma@hotmail.com>
2021-01-15 23:27:14 -0500

    Creating a README.md file

--- employeedirectory/src/utils/API.js
+++ employeedirectory/src/utils/API.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
-
 export default {
   getRandomEmployee: function() {
     return axios.get("https://randomuser.me/api/?inc=id,picture,name,phone,email,dob&results=50");
