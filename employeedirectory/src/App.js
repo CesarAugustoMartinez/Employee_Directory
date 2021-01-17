8e4fe2818d81a87f5e58d5908c117af8ec38f038
Cesar Augusto Martinez Auquilla <cesaguma@hotmail.com>
2021-01-16 19:09:23 -0500

    Update App.js

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import Employees from "./pages/Employees";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
