833fd3f49b61bf9e0bb3f71a69fd955f0a14f5a9
Cesar Augusto Martinez Auquilla <cesaguma@hotmail.com>
2021-01-16 19:13:25 -0500

    Adding a slint-diseble comment to handle a warning

--- employeedirectory/src/utils/API.js
+++ employeedirectory/src/utils/API.js
@@ -1,5 +1,5 @@
+/* eslint-disable import/no-anonymous-default-export */
 import axios from "axios";
-
 export default {
   getRandomEmployee: function() {
     return axios.get("https://randomuser.me/api/?inc=id,picture,name,phone,email,dob&results=50");
